20e056f7d77dbd75a9ced2b690ed36a8dc6d8bf3
Edward Viaene <ward@in4it.io>
2020-06-02 09:36:44 +0200

    return err to context

--- pkg/context/runoutput.go
+++ pkg/context/runoutput.go
@@ -16,7 +16,11 @@ type RunOutput struct {
 // Print prints the output using fmt.Printf
 func (r *RunOutput) Print() {
 	if r.err != nil {
-		panic(r.err)
+		if len(r.Contexts) == 0 || r.Contexts[0] == nil {
+			panic(r.err)
+		}
+		r.Contexts[0].err = r.err
+		return
 	}
 	key, value := r.GetKV()
 	for k := range key {
@@ -27,7 +31,11 @@ func (r *RunOutput) Print() {
 // Get retrieves all the values from the output
 func (r *RunOutput) Get() []types.RawOutput {
 	if r.err != nil {
-		panic(r.err)
+		if len(r.Contexts) == 0 || r.Contexts[0] == nil {
+			panic(r.err)
+		}
+		r.Contexts[0].err = r.err
+		return []types.RawOutput{}
 	}
 	ret := []types.RawOutput{}
 	for _, context := range r.Contexts {
@@ -50,7 +58,11 @@ func (r *RunOutput) Get() []types.RawOutput {
 // GetKV retrieves all key/value pairs from the output
 func (r *RunOutput) GetKV() ([]types.RawOutput, []types.RawOutput) {
 	if r.err != nil {
-		panic(r.err)
+		if len(r.Contexts) == 0 || r.Contexts[0] == nil {
+			panic(r.err)
+		}
+		r.Contexts[0].err = r.err
+		return []types.RawOutput{}, []types.RawOutput{}
 	}
 	keys := []types.RawOutput{}
 	values := []types.RawOutput{}
@@ -85,7 +97,11 @@ func (r *RunOutput) GetKV() ([]types.RawOutput, []types.RawOutput) {
 // The function passed to foreach is executed for every unique key.
 func (r *RunOutput) Foreach(fn types.ForeachFunction) {
 	if r.err != nil {
-		panic(r.err)
+		if len(r.Contexts) == 0 || r.Contexts[0] == nil {
+			panic(r.err)
+		}
+		r.Contexts[0].err = r.err
+		return
 	}
 	for _, context := range r.Contexts {
 		switch context.outputType {
